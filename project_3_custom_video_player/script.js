986e6f72b58b6b8c18092892b8b6ef2c6465ffc5
John Sims <jwsims816@gmail.com>
2022-01-30 16:48:02 -0600

    finished project 3 fixed CSS play vh bug

--- project_3_custom_video_player/script.js
+++ project_3_custom_video_player/script.js
@@ -16,7 +16,6 @@ function toggleVideoStatus() {
 // update play/pause icon
 function updatePlayIcon() {
   if (video.paused) {
-    
     play.innerHTML = '<i class="fa fa-play fa-2x"></i>';
   } else {
     play.innerHTML = '<i class="fa fa-pause fa-2x"></i>';
@@ -25,12 +24,25 @@ function updatePlayIcon() {
 
 // update progress & timestamp
 function updateProgress() {
-  return true;
+  progress.value = (video.currentTime / video.duration) * 100;
+
+  // Get minutes
+  let mins = Math.floor(video.currentTime / 60);
+  if (mins < 10) {
+    mins = "0" + String(mins);
+  }
+
+  let secs = Math.floor(video.currentTime % 60);
+  if (secs < 10) {
+    secs = "0" + String(secs);
+  }
+
+  timestamp.innerHTML = `${mins}:${secs}`
 }
 
 // set video time to progress
 function setVideoProgress() {
-  return true;
+  video.currentTime = (+progress.value * video.duration) / 100;
 }
 
 // stop Video
