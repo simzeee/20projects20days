6f7c28432b6939b263ad1ce7a59736355331b52f
John Sims <jwsims816@gmail.com>
2022-01-31 17:04:40 -0600

    finished JS

--- 4_Exchange_Rate_Calculator/script.js
+++ 4_Exchange_Rate_Calculator/script.js
@@ -13,16 +13,34 @@ function calculate() {
 
   fetch(
     `https://v6.exchangerate-api.com/v6/a0685ca0709a01c7372756ba/latest/${currency_one}`
-  ).then((res) => res.json())
-  .then(data => {
-    console.log(data)
-  })
+  )
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data);
+      const rate = data.conversion_rates[currency_two];
+      console.log(rate);
+
+      rateEl.innerText = `1 ${currency_one} == ${rate} ${currency_two}`;
+
+      amountEl_two.value = (amountEl_one.value * rate).toFixed(2);
+    });
 }
+
+
+
+
 
 // Event listeners
 currencyEl_one.addEventListener("change", calculate);
 amountEl_one.addEventListener("input", calculate);
 currencyEl_two.addEventListener("change", calculate);
 amountEl_two.addEventListener("input", calculate);
+
+swap.addEventListener('click', () => {
+  const temp = currencyEl_one.value
+  currencyEl_one.value = currencyEl_two.value
+  currencyEl_two.value = temp
+  calculate()
+})
 
 calculate();
