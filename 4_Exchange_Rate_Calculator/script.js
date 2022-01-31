a3d98bd7be2c95e65ee7b39bc38864b87a4e1490
John Sims <jwsims816@gmail.com>
2022-01-31 16:13:42 -0600

    got fetch request to work with my API key

---
+++ 4_Exchange_Rate_Calculator/script.js
@@ -0,0 +1,28 @@
+const currencyEl_one = document.getElementById("currency-one");
+const amountEl_one = document.getElementById("amount-one");
+const currencyEl_two = document.getElementById("currency-two");
+const amountEl_two = document.getElementById("amount-two");
+
+const rateEl = document.getElementById("rate");
+const swap = document.getElementById("swap");
+
+// Fetch exchange rates and update the DOM
+function calculate() {
+  const currency_one = currencyEl_one.value;
+  const currency_two = currencyEl_two.value;
+
+  fetch(
+    `https://v6.exchangerate-api.com/v6/a0685ca0709a01c7372756ba/latest/${currency_one}`
+  ).then((res) => res.json())
+  .then(data => {
+    console.log(data)
+  })
+}
+
+// Event listeners
+currencyEl_one.addEventListener("change", calculate);
+amountEl_one.addEventListener("input", calculate);
+currencyEl_two.addEventListener("change", calculate);
+amountEl_two.addEventListener("input", calculate);
+
+calculate();
