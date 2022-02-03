ecac08d1ea1a3c6c763965943ce86eac0be8f1ee
John Sims <jwsims816@gmail.com>
2022-02-02 22:58:57 -0600

    project complete

--- 5_DOM_Array_Methods/script.js
+++ 5_DOM_Array_Methods/script.js
@@ -14,7 +14,6 @@ getRandomUser();
 // fetch random user and add money
 
 async function getRandomUser() {
-
   const response = await fetch("https://randomuser.me/api");
   const data = await response.json();
 
@@ -22,12 +21,87 @@ async function getRandomUser() {
 
   const newUser = {
     name: `${user.name.first} ${user.name.last}`,
-    money: Math.floor(Math.random() * 1000000)
-  }
-  
+    money: Math.floor(Math.random() * 1000000),
+  };
+
   addData(newUser);
 }
 
-function addData(obj){
+function addData(obj) {
   data.push(obj);
+
+  updateDOM();
 }
+
+function doubleMoney() {
+  console.log(data);
+  data = data.map((user) => {
+    // let objClone = { ...obj }; // pass all key:value pairs from an object
+
+    console.log({ ...user });
+    return { ...user, money: user.money * 2 };
+  });
+
+  updateDOM();
+}
+
+function sortByRichest() {
+  data.sort((a, b) => b.money - a.money);
+  updateDOM();
+}
+
+// filter only millionaires
+
+function showMillionaires() {
+  console.log("ok");
+  data = data.filter((x) => x.money > 1000000);
+
+  updateDOM();
+}
+
+// add up all wealth
+
+function calculateWealth() {
+  const wealth = data.reduce((acc, user) => (acc += user.money), 0);
+
+  const wealthEl = document.createElement("div");
+  wealthEl.innerHTML = `<h3>Total Wealth: <strong>${formatMoney(
+    wealth
+  )}</strong></h3>`;
+
+  main.appendChild(wealthEl);
+
+  setTimeout(() => {
+    updateDOM()
+  }, 2000)
+}
+
+// Update DOM, if nothing is passed in, then we'll just use data
+function updateDOM(providedData = data) {
+  // Clear main div
+  main.innerHTML = "<h2><strong>Person</strong> Wealth</h2>";
+
+  providedData.forEach((item) => {
+    const element = document.createElement("div");
+    element.classList.add("person");
+    //this is cool cause you're seeing the call stack in action (I think)
+    element.innerHTML = `<strong>${item.name}</strong> ${formatMoney(
+      item.money
+    )}`;
+    main.appendChild(element);
+  });
+}
+
+// Format number as money
+
+function formatMoney(number) {
+  return "$" + number.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, "$&,");
+}
+
+// Event Listeners
+
+addUserBtn.addEventListener("click", getRandomUser);
+doubleBtn.addEventListener("click", doubleMoney);
+sortBtn.addEventListener("click", sortByRichest);
+showMillionairesBtn.addEventListener("click", showMillionaires);
+calculateWealthBtn.addEventListener("click", calculateWealth);
