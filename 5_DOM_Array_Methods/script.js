969c25c636690d8f256864b7c1be99d92df15925
John Sims <jwsims816@gmail.com>
2022-02-02 22:15:39 -0600

    first JS vid complete

---
+++ 5_DOM_Array_Methods/script.js
@@ -0,0 +1,33 @@
+const main = document.getElementById("main");
+const addUserBtn = document.getElementById("add-user");
+const doubleBtn = document.getElementById("double");
+const showMillionairesBtn = document.getElementById("show-millionaires");
+const sortBtn = document.getElementById("sort");
+const calculateWealthBtn = document.getElementById("calculate-wealth");
+
+let data = [];
+
+getRandomUser();
+getRandomUser();
+getRandomUser();
+
+// fetch random user and add money
+
+async function getRandomUser() {
+
+  const response = await fetch("https://randomuser.me/api");
+  const data = await response.json();
+
+  const user = data.results[0];
+
+  const newUser = {
+    name: `${user.name.first} ${user.name.last}`,
+    money: Math.floor(Math.random() * 1000000)
+  }
+  
+  addData(newUser);
+}
+
+function addData(obj){
+  data.push(obj);
+}
