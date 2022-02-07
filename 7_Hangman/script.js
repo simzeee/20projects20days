cbad455f3b623accfbb1f8aefdb10769c881776f
John Sims <jwsims816@gmail.com>
2022-02-07 13:27:52 -0600

    JS almost done

---
+++ 7_Hangman/script.js
@@ -0,0 +1,81 @@
+const wordEl = document.getElementById("word");
+const wrongLettersEl = document.getElementById("wrong-letters");
+const playAgainBtn = document.getElementById("play-again");
+const popup = document.getElementById("popup-container");
+const notification = document.getElementById("notification-container");
+const finalMessage = document.getElementById("final-message");
+
+console.log(notification)
+
+const figureParts = document.querySelectorAll(".figure-part");
+
+const words = ["application", "programming", "interface", "wizard"];
+
+let selectedWord = words[Math.floor(Math.random() * words.length)];
+
+const correctLetters = [];
+const wrongLetters = [];
+
+// Show hidden word
+function displayWord() {
+  wordEl.innerHTML = `
+    ${selectedWord
+      .split("")
+      .map(
+        (letter) =>
+          `<span class="letter">${
+            correctLetters.includes(letter) ? letter : ""
+          }</span>`
+      )
+      .join("")}
+  `;
+
+  const innerWord = wordEl.innerText.replace(/\n/g, "");
+  console.log(wordEl.innerText, innerWord);
+  if (innerWord === selectedWord) {
+    finalMessage.innerText = "Congratulations! You won! 🎉";
+    popup.style.display = "flex";
+  }
+}
+
+// Update the wrong letters
+function updateWrongLettersEl() {
+  console.log('Update wrong');
+}
+
+// Show notification
+
+function showNotification() {
+  notification.classList.add('show');
+
+  setTimeout(()=>{
+    notification.classList.remove('show');
+  }, 2000);
+}
+
+// Keydown letter press
+window.addEventListener("keydown", (e) => {
+  // console.log(e.keyCode)
+  if (e.keyCode >= 65 && e.keyCode <= 90) {
+    const letter = e.key;
+    if (selectedWord.includes(letter)) {
+      if (!correctLetters.includes(letter)) {
+        correctLetters.push(letter);
+        displayWord();
+      } else {
+        showNotification();
+      }
+    } else {
+      if (!wrongLetters.includes(letter)) {
+        wrongLetters.push(letter);
+
+        updateWrongLettersEl();
+      }
+      else{
+        showNotification()
+      }
+    }
+  }
+});
+
+displayWord();
