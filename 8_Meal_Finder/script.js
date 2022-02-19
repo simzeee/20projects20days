6e4ab5c294ca23fe1a42e0b24a6beabad8f7caf0
John Sims <jwsims816@gmail.com>
2022-02-19 15:36:33 -0600

    individual item JS complete

--- 8_Meal_Finder/script.js
+++ 8_Meal_Finder/script.js
@@ -31,7 +31,7 @@ function searchMeal(e) {
             .map(
               (meal) =>
                 `<div class="meal">
-            <img src="${meal.strMealThumb}" alt="${meals.strMeal}"/>
+            <img src="${meal.strMealThumb}" alt="${meal.strMeal}"/>
             <div class="meal-info" data-mealID="${meal.idMeal}">
             <h3>${meal.strMeal}</h3>
             </div>
@@ -40,14 +40,79 @@ function searchMeal(e) {
             .join("");
         }
       });
-      // Clear search text
-      search.value = "";
-
+    // Clear search text
+    search.value = "";
   } else {
     alert("Please enter a search term");
   }
 }
 
+// Fetch meal by ID
+function getMealById(mealID) {
+  fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealID}`)
+    .then((res) => res.json())
+    .then((data) => {
+      const meal = data.meals[0];
+
+      addMealToDom(meal);
+    }
+    );
+}
+
+// Add meal to DOM
+
+function addMealToDom(meal){
+  const ingredients = [];
+
+  for(let i = 1; i <=20; i++){
+    if (meal[`strIngredient${i}`]){
+      ingredients.push(`${meal[`strIngredient${i}`]} - ${meal[`strMeasure${i}`]}`)
+    }
+    else{
+      break;
+    }
+  }
+
+  single_mealEl.innerHTML = `
+  <div class="single-meal">
+  <h1>
+  ${meal.strMeal}
+  </h1>
+  <img src="${meal.strMealThumb}" alt="${meal.strMealThumb}"/>
+  <div class="single-meal-info">
+  ${meal.strCategory ? `<p>${meal.strCategory}</p>` : ""}
+  ${meal.strArea ? `<p>${meal.strArea}</p>` : ""}
+  </div>
+  <div class="main">
+  <p>${meal.strInstructions}</p>
+  <h2>Ingredients</h2>
+  <ul>
+  ${ingredients.map(ing => `<li>${ing}</li>`).join('')}
+  </ul>
+  </div>
+  </div>`;
+
+}
+
 // Event listeners
 
 submit.addEventListener("submit", searchMeal);
+
+// What is event .path?
+
+mealsEl.addEventListener("click", (e) => {
+  const mealInfo = e.path.find((item) => {
+    if (item.classList) {
+      return item.classList.contains("meal-info");
+    } else {
+      return false;
+    }
+  });
+  console.log(mealInfo);
+
+  if (mealInfo) {
+    const mealID = mealInfo.getAttribute("data-mealid");
+    console.log(mealID);
+    getMealById(mealID);
+  }
+});
