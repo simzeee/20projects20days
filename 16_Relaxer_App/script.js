851f2eade7388a310043c0adb778dd61909f936f
John Sims <jwsims816@gmail.com>
2022-08-05 10:23:13 -0400

    JS added and relaxer app complete

---
+++ 16_Relaxer_App/script.js
@@ -0,0 +1,22 @@
+const container = document.getElementById("container");
+const text = document.getElementById("text");
+
+const totalTime = 7500;
+const breatheTime = (totalTime / 5) * 2;
+const holdTime = totalTime / 5;
+
+breathAnimation();
+
+function breathAnimation() {
+  text.innerText = 'Breath in!'
+  container.className = 'container grow'
+  setTimeout(() => {
+    text.innerText = 'Hold'
+    setTimeout(() => {
+      text.innerText = 'Breath out!'
+      container.className = 'container shrink'
+    }, holdTime);
+  }, breatheTime);
+}
+
+setInterval(breathAnimation, totalTime)
